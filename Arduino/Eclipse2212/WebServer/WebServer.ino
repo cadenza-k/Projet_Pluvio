e5c7da6eb63a87586d3d1747fc8239bdfd5c1b38
Eclipse2212 <lucas.assier33@gmail.com>
2016-04-27 14:52:36 +0200

    Add WebServer

---
+++ Arduino/Eclipse2212/WebServer/WebServer.ino
@@ -0,0 +1,60 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
+
+void setup()
+{
+    Ethernet.begin(mac, ip);  // initialize Ethernet device
+    server.begin();           // start to listen for clients
+}
+
+void loop()
+{
+    EthernetClient client = server.available();  // try to get client
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read(); // read 1 byte (character) from client
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: close");
+                    client.println();
+                    // send web page
+                    client.println("<!DOCTYPE html>");
+                    client.println("<html>");
+                    client.println("<head>");
+                    client.println("<title>Arduino Web Page</title>");
+                    client.println("</head>");
+                    client.println("<body>");
+                    client.println("<h1>Hello from Arduino!</h1>");
+                    client.println("<p>A web page from the Arduino server</p>");
+                    client.println("</body>");
+                    client.println("</html>");
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    // last character on line of received text
+                    // starting new line with next character read
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    // a text character was received from client
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // give the web browser time to receive the data
+        client.stop(); // close the connection
+    } // end if (client)
+}
