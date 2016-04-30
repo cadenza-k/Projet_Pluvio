d4bce09141c9b5895d795f3c1df0c662d35d7f7f
Eclipse2212 <lucas.assier33@gmail.com>
2016-04-30 12:00:57 +0200

    Fichier comenté

--- Site/Table_Loader_Before_Yesterday.js
+++ Site/Table_Loader_Before_Yesterday.js
@@ -1,17 +1,17 @@
-$(document).ready(
+$(document).ready(  //Dès que le document est prêt, on lance ce qui est en dessous
  function() 
  {
-var Moment = moment();
-Moment.locale('fr');
-var Jour = Moment.subtract(2, 'Days').format("D");
-var Mois = Moment.format("MM");
-var Annee = Moment.format("Y");
-var FormatFichier = Jour + '_' + Mois + '_' + Annee + '.xml';
+var Moment = moment();  //On charge la méthode princiupale de Moment.js
+Moment.locale('fr');    //On règle la langue de la date en français au cas où
+var Jour = Moment.subtract(2, 'Days').format("D");//On récupère le jour précédent
+var Mois = Moment.format("MM"); //On récupère le mois
+var Annee = Moment.format("Y"); //On récupère l'année
+var FormatFichier = Jour + '_' + Mois + '_' + Annee + '.xml';   //On concatène le tout en une variable qui correspond au nom du fichier d'hier
 
 
-$.ajax( {
+$.ajax( {   //On va chercher le fichier grâce à AJAX
             type: "GET",
-            url: FormatFichier,
+            url: FormatFichier, //Le nom de fichier est fourni par la variable "FormatFichier"
             dataType: "xml",
             success: function(xml) 
                      {
