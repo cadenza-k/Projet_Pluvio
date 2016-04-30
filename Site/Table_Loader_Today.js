eef55bf9822fee30f6d3d27d2351f5dfef822a39
Eclipse2212 <lucas.assier33@gmail.com>
2016-04-30 17:22:49 +0200

    \o/ ça marche!!
    Le tableau est enfin centré + un total en Jaune en bas

--- Site/Table_Loader_Today.js
+++ Site/Table_Loader_Today.js
@@ -7,6 +7,7 @@ var Jour = Moment.format("D");//On récupère le jour précédent
 var Mois = Moment.format("MM"); //On récupère le mois
 var Annee = Moment.format("Y"); //On récupère l'année
 var FormatFichier = Jour + '_' + Mois + '_' + Annee + '.xml';   //On concatène le tout en une variable qui correspond au nom du fichier d'hier
+var Data = 0;
 
 
 $.ajax( {   //On va chercher le fichier grâce à AJAX
@@ -20,8 +21,10 @@ $.ajax( {   //On va chercher le fichier grâce à AJAX
                          {
                             var id = $(this).attr('id');
                             var data = $(this).find('data').text();
-                            $("#Pluvio > tbody:last").append('<tr><td>' + id + '</td><td>' + data + '</td></tr>');
+                             Data= Data + parseInt(data);
+                            $("#Pluvio > tbody:last").append('<tr><td>' + id + '</td><td>' + data + 'mm</td></tr>');
                          });
+                         $("#Pluvio > tbody:last").append('<tr style="background-color: #ffffcc;"><td>TOTAL</td><td>' + Data + 'mm</td></tr>');
                       }
         });
  });
