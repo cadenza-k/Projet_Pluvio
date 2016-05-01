7e3fd319fa6c1d838e85ffb2ed125086ff607ab9
Eclipse2212 <lucas.assier33@gmail.com>
2016-05-01 18:39:05 +0200

    J'ai fais de la merde?

--- Site/Test_Mois.js
+++ Site/Test_Mois.js
@@ -8,38 +8,35 @@ var Jour = Moment.format("D");//On récupère le jour
 var Mois = Moment.format("MM"); //On récupère le mois
 var Annee = Moment.format("Y"); //On récupère l'année
 var FormatFichier = Jour + '_' + Mois + '_' + Annee + '.xml';   //On concatène le tout en une variable qui correspond au nom du fichier d'hier
-
 var NomJour = Moment.format("ddd"); //On récup' le jour sous forme 3lettres puis . (ex: "Lun.")
 var JTable = NomJour + ' ' + Jour; // on assemble les deux
 var Data = 0;
-     
+var data = 0;
+
 while(Jour > 0)
     {
-        FetchFile();
-         $("#Pluvio > tbody:last").append('<tr><td>' + JTable + '</td><td>' + Data + 'mm</td></tr>'); //Ajoute du contenu juste avant la dernière balise <tbody>
-        console.log(Data);
-        Data = 0;
+        $("#Pluvio > tbody:last").append('<tr><td>' + JTable + '</td><td>' + Data + 'mm</td></tr>'); //Ajoute du contenu juste avant la dernière balise <tbody>
+//        Data = 0;
         Jour--;
         FormatFichier = Jour + '_' + Mois + '_' + Annee + '.xml';
         NomJour = Moment.subtract(1, 'Days').format("ddd");
         var JTable = NomJour + ' ' + Jour;
-        
-    }
 
-function FetchFile(){
   $.ajax( {
             type: "GET",
             url: FormatFichier,
             dataType: "xml",
-            success: function(xml) 
+            success: function(xml)
                      {
                        $(xml).find('heure').each(   //Pour chaque balise <heure></heure>, faire
                          function()
                          {
-                            var data = $(this).find('data').text();
-                             Data= Data + parseInt(data);
+                            data = $(this).find('data').text();
+                            Data= Data + parseInt(data);
                          });
+                         console.log(Data);
+      
 					 }
-        });  
-}
+        });
+    }
 });
