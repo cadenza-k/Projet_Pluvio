d4f7a7d5a4086aec0f0429596cb92c14467e0a09
Eclipse2212 <lucas.assier33@gmail.com>
2016-05-06 15:37:19 +0200

    Dernières modifs avant la fin

--- Site/Table_Loader_Today.js
+++ Site/Table_Loader_Today.js
@@ -1,11 +1,11 @@
 $(document).ready(  //Dès que le document est prêt, on lance ce qui est en dessous
  function() 
  {
-var Moment = moment();  //On charge la méthode princiupale de Moment.js
-Moment.locale('fr');    //On règle la langue de la date en français au cas où
-var Jour = Moment.format("D");//On récupère le jour précédent
-var Mois = Moment.format("MM"); //On récupère le mois
-var Annee = Moment.format("Y"); //On récupère l'année
+var M = moment();  //On charge la méthode princiupale de Moment.js
+M.locale('fr');    //On règle la langue de la date en français au cas où
+var Jour = M.format("D");//On récupère le jour précédent
+var Mois = M.format("MM"); //On récupère le mois
+var Annee = M.format("Y"); //On récupère l'année
 var FormatFichier = Jour + '_' + Mois + '_' + Annee + '.xml';   //On concatène le tout en une variable qui correspond au nom du fichier d'hier
 var Data = 0;
 
@@ -14,18 +14,17 @@ $.ajax( {   //On va chercher le fichier grâce à AJAX
             type: "GET",
             url: FormatFichier, //Le nom de fichier est fourni par la variable "FormatFichier"
             dataType: "xml",
-            error: alert('Fichier non trouvé'),
             success: function(xml) 
-                     {
-                       $(xml).find('heure').each(   
-                         function()
-                         {
-                            var id = $(this).attr('id');
-                            var data = $(this).find('data').text();
-                             Data= Data + parseInt(data);
-                            $("#Pluvio > tbody:last").append('<tr><td>' + id + '</td><td>' + data + 'mm</td></tr>');
-                         });
-                         $("#Pluvio > tbody:last").append('<tr style="background-color: #ffffcc;"><td>TOTAL</td><td>' + Data + 'mm</td></tr>');
-                      }
+                {
+                    $(xml).find('heure').each( //pour chaque duo de baslises <heure>,  
+                    function()
+                    {
+                        var id = $(this).attr('id'); //Récupérer l'arrtibut ID
+                        var data = $(this).find('data').text(); //Récupérer la valeur contenue entre les balises <data>
+                        Data= Data + parseInt(data); //On additione les données entre-elles
+                        $("#Pluvio > tbody:last").append('<tr><td>' + id + ' h</td><td>' + data + 'mm</td></tr>');
+                    });
+                    $("#Pluvio > tbody:last").append('<tr style="background-color: #ffffcc;"><td>TOTAL</td><td>' + Data + 'mm</td></tr>');
+                }
         });
  });
